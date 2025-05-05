3e027b0988a94f280ccbf8de4212d0e52a0cbe32
andrii-smahin-moc <andrii.smahin@masterofcode.com>
2025-05-05 22:20:55 +0300

    updated render scheduler

--- src/functions/main.js
+++ src/functions/main.js
@@ -24,3 +24,17 @@ function loadPage(pageName) {
 
   return '<p>Сторінку "' + pageName + '" не знайдено.</p>';
 }
+
+function getScheduleForDay(day) {
+  var sheet = SpreadsheetApp.openById(
+    "1Pqx0UDzGQMjl6G0iZtcp5ftiv2YeuOQ8xqn6nYM3_A4"
+  ).getSheetByName("Лист1");
+  var data = sheet.getDataRange().getValues();
+  if (data.length < 2) return [];
+
+  var filtered = data.filter(function (row, i) {
+    return i > 0 && row[0] === day;
+  });
+
+  return filtered;
+}
