53ac301b9942295cc44945a2cc3ec506ccdbf391
andrii-smahin-moc <andrii.smahin@masterofcode.com>
2025-05-05 14:43:46 +0300

    update config

--- build.js
+++ build.js
@@ -2,13 +2,9 @@ const fs = require("fs-extra");
 const { execSync } = require("child_process");
 
 async function build() {
-  console.log("[BUILD] Webpack bundling...");
-  execSync("npx webpack", { stdio: "inherit" });
-
   console.log("[COPY] Copy to dist...");
   fs.emptyDirSync("dist");
-  fs.copySync("src/functions", "dist");
-  fs.copyFileSync("bundled/ui.html", "dist/ui.html");
+  fs.copySync("src", "dist");
 
   console.log("[PUSH] clasp push...");
   execSync("npx clasp push", { stdio: "inherit" });
